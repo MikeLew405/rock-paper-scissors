9fa0389267f5b57e452978e62c85b80004cadd10
Mike Lewis <59457355+MikeLew405@users.noreply.github.com>
2023-12-01 16:18:49 -0500

    completed first draft of firstRound method

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -13,8 +13,38 @@ function getComputerSelection() {
 }
 
 function playRound(computerSelection, userSelection) {
-
-
+    console.log(`user selection = ${userSelection}, computer selection = ${computerSelection}`);
+    if (userSelection == "rock") {
+        if (computerSelection == userSelection) {
+            console.log("TIE! Restarting round");
+            return(playRound(getComputerSelection(), prompt("Pick: Rock, Paper, or Scissors?").toLowerCase()));
+        } else if (computerSelection == "paper") {
+            return("You lose! paper beats rock");
+        } else {
+            return("You win! Rock beats Scissors");
+        }
+    } else if (userSelection == "paper") {
+        if (computerSelection == userSelection) {
+            console.log("TIE! Restarting round");
+            return(playRound(getComputerSelection(), prompt("Pick: Rock, Paper, or Scissors?").toLowerCase()));
+        } else if (computerSelection == "scissors") {
+            return("You lose! Scissors beats Paper");
+        } else {
+            return("You win! Paper beats Rock");
+        }
+    }
+    else if(userSelection == "scissors") {
+        if (computerSelection == userSelection) {
+            console.log("TIE! Restarting round");
+            return(playRound(getComputerSelection(), prompt("Pick: Rock, Paper, or Scissors?").toLowerCase()));
+        } else if (computerSelection == "rock") {
+            return("You lose! Rock beats Scissors");
+        } else {
+            return("You win! Scissors beats Rock");
+        }
+    } else {
+        console.log("Oops, something went terribly wrong getting the user's selection!");
+    }
 }
 
-playRound(getComputerSelection(), prompt("Pick: Rock, Paper, or Scissors?").toLowerCase);
+console.log(playRound(getComputerSelection(), prompt("Pick: Rock, Paper, or Scissors?").toLowerCase()));
